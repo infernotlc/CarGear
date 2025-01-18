d8ccec8be1b10b08cd3974dacfd75cc169741b76
infernotlc <talha5797@gmail.com>
2025-01-18 21:03:22 +0300

    feat:new

--- ui-module/src/main/java/com/example/ui_module/screen/GearScreen.kt
+++ ui-module/src/main/java/com/example/ui_module/screen/GearScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
