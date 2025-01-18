d441b2db5c9cd9f5b426e7675e78f042aca54bf8
infernotlc <talha5797@gmail.com>
2025-01-18 20:45:32 +0300

    feat:settings

--- gear-module/build.gradle.kts
+++ gear-module/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.gear_module"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.gear_module"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
